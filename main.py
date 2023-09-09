d31a75d5482ca449af1297400b24e5bb74e9fb7d
Maksmvs <99.mvs.99@gmail.com>
2023-09-09 14:38:02 +0300

    N3

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ print("Hello World")
 
 
 print("Hello Git")
+
+name = input("What is your name? ")
+print(f"Hello {name}")
+
+age = input("How old are you? ")
+
+if int(age) >= 18:
+    print("You are adult already.")
+else:
+    print("You are infant yet.")
